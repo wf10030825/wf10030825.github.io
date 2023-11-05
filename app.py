c2f98b89b8b834dfe8be8a873ae8d36653b27b9e
wf10030825 <147973594+wf10030825@users.noreply.github.com>
2023-11-05 15:21:56 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
 
+with open('data.html', 'r', encoding='utf-8') as text_file:
+    text_content = text_file.read()
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def upload():
+    return '{} '.format(text_content)
+
 
 if __name__ == '__main__':
     app.run()
